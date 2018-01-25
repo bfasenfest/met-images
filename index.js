221dad07de59e71efaa419d92460d4f40102245b
bfasenfest <GEMPA@dyn-128-59-165-173.dyn.columbia.edu>
2018-01-24 20:30:08 -0500

    test image processing

--- index.js
+++ index.js
@@ -7,17 +7,47 @@ var json = require('./metObjects.json');
 
 var itemsBeingProcessed = 0;
 var fileQueue = [];
-var maxItems = 30
+var maxItems = 10
 var counter = 0
 
-var outputName = "metObjects-linkedall.json"
+var outputName = "tiles2.json"
 var max = json.length || 2000
+const exportFile = true
 
 var t0 = now(), t1 = 0
 
-json.forEach( (artwork, index, array) => {
+tiles = json.filter(artwork => {
+  if (artwork["Object Name"]) return artwork["Object Name"].includes("Tile")
+  else return false
+})
+
+tiles.forEach( (artwork, index, array) => {
   processUrl(artwork)
 })
+
+// counter = 0
+
+// tiles.forEach( (artwork, index, array) => {
+//   processImage(artwork, './images/')
+
+//   process.stdout.write(` \r processed ${counter} of ${array.length} `);
+
+// })
+
+
+// tiles.forEach( (artwork, index, array) => {
+//   // processUrl(artwork)
+//   if (artwork["Object Name"] == "Tile"){
+//     processImage(artwork, './images/')
+//   }
+
+//   process.stdout.write(` \r processed ${counter} of ${index} `);
+
+// })
+
+// json.forEach( (artwork, index, array) => {
+//   processUrl(artwork)
+// })
 
 
 function processUrl(artwork) {
@@ -59,9 +89,70 @@ function processUrl(artwork) {
 }
 
 function outputFile(){
+  if (!exportFile) return
   console.log('...Outputing file...')
   t1 = now()
   let secs = ((t1-t0)/1000).toFixed(1)
   console.log("Output took " + secs + " seconds ( " + secs/60 + " minutes)")
   fs.writeFileSync(outputName, JSON.stringify(json)); // json.splice(0,counter))
+  linksFound = true;
+}
+
+function saveImage(artwork, folder){
+  let link = artwork["imgSrc"]
+  let name = artwork["Title"]
+  let id = artwork["Object ID"]
+  request(link).pipe(fs.createWriteStream(folder + name + "_" + id + '.png'));
+  counter++
+  finishImage()
+}
+
+function processImage(artwork, folder = './') {
+  if (itemsBeingProcessed > maxItems) {
+    fileQueue.push(artwork);
+    return;
+  }
+  let page = artwork["Link Resource"]
+
+  itemsBeingProcessed += 1;
+
+  if(!artwork.imgSrc){
+    request(page, function (error, response, html) {
+      if (!error && response.statusCode == 200) {
+        artwork.imgSrc = HTMLtoLink(html)
+        saveImage(artwork, folder)
+      }
+      else {
+        reject(Error("It broke"));
+        artwork.imgSrc = ''
+        saveImage(artwork, folder)
+      }
+    })
+  }
+  else{
+    saveImage(artwork, folder)
+  }
+
+  // console.log(artwork.imgSrc)
+
+
+
+  // saveImage(artwork, folder)
+
+}
+
+function finishImage() {
+  itemsBeingProcessed -= 1;
+  if (itemsBeingProcessed <= maxItems && fileQueue.length > 1) {
+    processUrl(fileQueue.shift());
+  }
+}
+
+function HTMLtoLink(html){
+  var $ = cheerio.load(html)
+  var icon = $('.icon--download')
+  var a = icon.parent()
+  var href = a.attr('href')
+  var parsed = href ? href.replace("{{selectedOrDefaultDownload('","").replace("')}}","") : ""
+  return parsed
 }

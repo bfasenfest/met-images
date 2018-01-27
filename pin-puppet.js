d53c6dbffa16e8898cba97bb0ff4820d0e997da2
bfasenfest <GEMPA@dyn-128-59-165-173.dyn.columbia.edu>
2018-01-27 18:21:14 -0500

    puppet working!

--- pin-puppet.js
+++ pin-puppet.js
@@ -1,15 +1,86 @@
 const puppeteer = require('puppeteer');
+var clear       = require('clear');
+
+
+clear();
 
 async function browse() {
+  height = 1000
+  width = 1600
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
+  await page.setViewport({width, height})
 
-  await page.goto('https://www.pinterest.com/search/boards/?q=dubuffet');
+  const {targetInfos: [{targetId}]} = await browser._connection.send(
+    'Target.getTargets'
+  );
+  
+  // Tab window. 
+  const {windowId} = await browser._connection.send(
+    'Browser.getWindowForTarget',
+    {targetId}
+  );
+
+
+  await browser._connection.send('Browser.setWindowBounds', {
+    bounds: {height, width},
+    windowId
+  })
+  await page.goto('https://www.pinterest.com/search/pins/?q=sarah%20charlesworth');
   await page.waitFor(1000);
-  await page.click(body > div:nth-child(3) > div > div:nth-child(1) > div > div > div.appContent > div > div.SearchPage > div > div > div > div:nth-child(1) > div > div > div > div > a)
-  await page.screenshot({ path: 'screenshots/dubuffet.png' });
 
-  browser.close();
+  // var data = await page.evaluate(() => {
+  //   let json = document.querySelector('#jsInit1') //('div.GrowthUnauthPin_brioPin')
+  //   return json.innerHTML
+  // })
+
+  // json = json.outerHTML
+
+  let images = []
+  for (i = 1; i <= 100; i++){
+    let sel = 'body > div.App.AppBase.Module > div.appContent > div.mainContainer > div > div > div > div > div:nth-child(2) > div > div > div > div:nth-child(' + i + ') > div > div.GrowthUnauthPinImage > a > img'
+    let image = await page.evaluate((i, sel) => {
+      let json = document.querySelector(sel) //('div.GrowthUnauthPin_brioPin')
+      json = json.getAttribute('src').replace(/236x/, 'originals');
+      return json
+    }, i, sel)
+    if (i % 20 == 0) {
+      await page.hover(sel)
+      await page.waitFor(1000)
+    }
+    console.log(image)
+    images.push(image)
+}
+
+  // var image = await page.evaluate(() => {
+  //   let json = document.querySelector('body > div.App.AppBase.Module > div.appContent > div.mainContainer > div > div > div > div > div:nth-child(2) > div > div > div > div:nth-child(2) > div > div.GrowthUnauthPinImage > a > img') //('div.GrowthUnauthPin_brioPin')
+  //   json = json.getAttribute('src')
+  //   return json
+  // })
+
+
+
+  // json = JSON.parse(json)
+  // console.log(image)
+  // let results = json.resourceDataCache[0].data.results
+  // let images = []
+  // results.forEach ( (result) => {
+  //   let image = result.images.orig.url
+  //   images.push(image)
+  // })
+  // console.log(images)
+
+
+  // First grid item for pins is 55
+  // await page.click('body > div.App.AppBase.Module > div.appContent > div.mainContainer > div > div > div > div > div:nth-child(2) > div > div > div > div:nth-child(55)') //('div.GrowthUnauthPin_brioPin')
+  // await page.screenshot({ path: 'dubuffet.png' });
+  // await page.waitFor(1000);
+  // await page.goto('https://www.pinterest.com/search/boards/?q=dubuffet');
+  // await page.click('body > div.App.AppBase.Module > div.appContent > div.mainContainer > div > div > div > div > div:nth-child(2) > div > div > div > div:nth-child(56)') //('div.GrowthUnauthPin_brioPin')
+
+
+
+  // browser.close();
 }
 
 browse();
